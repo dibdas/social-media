262a7756f0b872f4f498be8a18ed4a8a5df93c95
dibdas <dibyandu08das@gmail.com>
2023-01-05 19:13:33 +0530

    added file

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -7,5 +7,7 @@ const {
 const router = require("express").Router();
 router.post("/signup", signupController);
 router.post("/login", loginController);
-router.post("/refresh", refreshAccessTokenController);
+// router.post("/refresh", refreshAccessTokenController);
+// transforming '/refresh' into GET request as we are not passing the refresh token in the body JSON
+router.get("/refresh", refreshAccessTokenController);
 module.exports = router;
