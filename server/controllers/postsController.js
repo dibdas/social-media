50a4b6f5baee51996c58f1a2a1b9bf5c1e172323
dibdas <dibyandu08das@gmail.com>
2023-01-05 19:12:40 +0530

    modified

--- server/controllers/postsController.js
+++ server/controllers/postsController.js
@@ -1,5 +1,8 @@
+const { error, success } = require("../utils/responseWrapper");
+
 const getAllPostsController = async (req, res) => {
   console.log(req._id);
-  return res.json({ text: `these are all the routes` });
+  // return res.json({ text: `these are all the routes` });
+  return res.send(success(200, { message: `these are all the routes` }));
 };
 module.exports = { getAllPostsController };
