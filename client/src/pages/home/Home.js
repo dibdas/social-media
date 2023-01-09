23bd532da0ce74087c5207a6f563bebb5aaf1ebf
dibdas <dibyandu08das@gmail.com>
2023-01-09 18:00:55 +0530

    modified

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -3,13 +3,17 @@ import { Link } from "react-router-dom";
 import axiosClient from "../../utils/axiosClient";
 
 function Home() {
-  useEffect(() => {
-    fetchdata();
-  }, []);
-  async function fetchdata() {
-    const response = await axiosClient.get("/post/all");
-    console.log(` got the response ${response}`);
-  }
+  // async function fetchdata() {
+  //   try {
+  //     const response = await axiosClient.get("/post/all");
+  //     console.log(` got the response ${response}`);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
+  // useEffect(() => {
+  //   fetchdata();
+  // }, []);
   return (
     <div>
       <Link to="/login">Log In</Link>
