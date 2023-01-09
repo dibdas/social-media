f6131b86f866e91d9656f72d887adc14241e9fc7
dibdas <dibyandu08das@gmail.com>
2023-01-09 18:00:36 +0530

    modified

--- client/src/pages/signup/SignUp.js
+++ client/src/pages/signup/SignUp.js
@@ -1,25 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import axiosClient from "../../utils/axiosClient";
 import "./SignUp.scss";
 
 function SignUp() {
+  const [email, setEmail] = useState();
+  const [password, setpassword] = useState();
+  const [name, setName] = useState();
+  async function handleSubmit(event) {
+    event.preventDefault();
+    try {
+      const result = await axiosClient.post("/auth/signup", {
+        name,
+        email,
+        password,
+      });
+      console.log(result);
+    } catch (error) {
+      console.log(error);
+    }
+  }
   return (
     <div className="signup">
       <div className="signup-box">
         <h2 className="heading">SgnUp</h2>
-        <form action="">
+
+        <form action="" onSubmit={handleSubmit}>
           {/* providing id to the input field is important,as id can be linked to label */}
 
           <label htmlFor="name">Name</label>
-          <input type="text" className="name" id="name" />
+          <input
+            type="text"
+            className="name"
+            id="name"
+            onChange={(e) => setName(e.target.value)}
+          />
 
           <label htmlFor="email">Email</label>
-          <input type="email" className="email" id="email" />
+          <input
+            type="email"
+            className="email"
+            id="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
 
           <label htmlFor="password">Password</label>
-          <input type="password" className="password" id="password" />
+          <input
+            type="password"
+            className="password"
+            id="password"
+            onChange={(e) => setpassword(e.target.value)}
+          />
 
-          <button type="submit">Sign Up</button>
+          <button type="submit" onSubmit={handleSubmit}>
+            Sign Up
+          </button>
         </form>
         <p className="signUp">
           Already have an account? <Link to="/login">Login</Link>
