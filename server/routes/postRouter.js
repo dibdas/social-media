6b8b49a6be847fb72a511b7f0dc76eb719d5880e
dibdas <dibyandu08das@gmail.com>
2023-01-10 14:27:34 +0530

    modified

--- server/routes/postRouter.js
+++ server/routes/postRouter.js
@@ -1,7 +1,15 @@
-const { getAllPostsController } = require("../controllers/postsController");
+const {
+  likeandUnlikePostController,
+} = require("../controllers/likesAndUnlike");
+const {
+  getAllPostsController,
+  createPostController,
+} = require("../controllers/postsController");
 const userRequire = require("../middlewares/requireUser");
 
 const router = require("express").Router();
 // when userRequire hit next function or call the next function, then getAllPostsController will be called
 router.get("/all", userRequire, getAllPostsController);
+router.post("/new", userRequire, createPostController);
+router.post("/like", userRequire, likeandUnlikePostController);
 module.exports = router;
