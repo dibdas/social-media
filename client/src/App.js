04ca41a99b748223bda2f07aad30e22b95fddc0f
dibdas <dibyandu08das@gmail.com>
2023-01-12 20:24:37 +0530

    added new files

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import { Route, Routes } from "react-router-dom";
 import SignUp from "./pages/signup/SignUp";
 import Home from "./pages/home/Home";
 import RequireUser from "./components/RequireUser";
+import Feed from "./components/feed/Feed";
+import Profile from "./components/profile/Profile";
 
 function App() {
   return (
@@ -14,7 +16,19 @@ function App() {
           {/* if we need to go to home, we need to go to require user , require user will check whether
           access token is there or not , if access token is there , we sent you to home,
           orherwise we sent you to login page   */}
-          <Route path="/" element={<Home />} />
+          {/* <Route path="/" element={<Home />}>  */}
+          {/* path="/" inside  <Route path="/" element={<Home />}>  is not necessary  */}
+          <Route element={<Home />}>
+            {/* when "/" it will got user Require if the require user exist then it goes to Home,
+            Home will make the Navbar and Outlet , and inside that Outlet the Feed component will open */}
+            <Route path="/" element={<Feed />} />
+            {/* when path is "/profile/:userId" it will go to UserRequire , if the
+            user exist i.e the user Require exist , then it will go to Home
+            component , the Home component will make the navbar as defined in
+            the Home component , and then inside the outlet the Profile
+            component will open */}
+            <Route path="/profile/:userId" element={<Profile />} />
+          </Route>
         </Route>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,24 +1,27 @@
 import React, { useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
+import Navbar from "../../components/navbar/Navbar";
 import axiosClient from "../../utils/axiosClient";
 
 function Home() {
-  async function fetchdata() {
-    try {
-      const response = await axiosClient.get("/post/all");
-      console.log(` got the response ${response}`);
-    } catch (error) {
-      console.log(error);
-    }
-  }
-  useEffect(() => {
-    fetchdata();
-  }, []);
+  // async function fetchdata() {
+  //   try {
+  //     const response = await axiosClient.get("/post/all");
+  //     console.log(` got the response ${response}`);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
+  // useEffect(() => {
+  //   fetchdata();
+  // }, []);
   return (
-    <div>
-      <Link to="/login">Log In</Link>
-      <Link to="/signup">Sign Up</Link>
-    </div>
+    <>
+      {/* <Link to="/login">Log In</Link>
+      <Link to="/signup">Sign Up</Link> */}
+      <Navbar />
+      <Outlet />
+    </>
   );
 }
 
