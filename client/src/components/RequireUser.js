39ec84742e0ad1c6acf1454e40d4617334b42d3f
dibdas <dibyandu08das@gmail.com>
2023-01-12 19:21:32 +0530

    added new files

---
+++ client/src/components/RequireUser.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Navigate, Outlet } from "react-router-dom";
+import {
+  gettingItemFromLocalStorage,
+  KEY_ACCESS_TOKEN,
+} from "../utils/localStorageManager";
+// this file is to check whether the user logged in or not
+function RequireUser() {
+  {
+    /* if we need to go to home, we need to go to require user , require user will check whether
+          access token is there or not , if access token is there , we sent you to home,
+orherwise we sent you to login page  */
+  }
+  // if we do have the access tken key then the user is loggedin
+  const user = gettingItemFromLocalStorage(KEY_ACCESS_TOKEN);
+  return user ? <Outlet /> : <Navigate to="/login" />;
+}
+
+export default RequireUser;
