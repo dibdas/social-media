43e000f98b13cd12eb8a595168222c7923623c92
dibdas <dibyandu08das@gmail.com>
2023-01-14 02:17:06 +0530

    added styles

--- client/src/components/followers/Follower.js
+++ client/src/components/followers/Follower.js
@@ -1,7 +1,17 @@
 import React from "react";
+import Avatar from "../avatar/Avatar";
+import "./Follower.scss";
 
 function Follower() {
-  return <div>Follower</div>;
+  return (
+    <div className="Follower">
+      <div className="user-info">
+        <Avatar />
+        <h4 className="name">Robin</h4>
+      </div>
+      <h5 className="hover-link follow-link">Follow</h5>
+    </div>
+  );
 }
 
 export default Follower;
