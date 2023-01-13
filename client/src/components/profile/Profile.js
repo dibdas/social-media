8923433baa5d657e8d1057554b20179e682bcf0f
dibdas <dibyandu08das@gmail.com>
2023-01-14 02:17:41 +0530

    added styles

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,7 +1,40 @@
 import React from "react";
+import Post from "../post/Post";
+import "./Profile.scss";
+import userImg from "../../assets/superhero.png";
+import { useNavigate } from "react-router-dom";
 
 function Profile() {
-  return <div>Profile</div>;
+  const navigate = useNavigate();
+  return (
+    <div className="Profile">
+      <div className="container">
+        <div className="left-side">
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+        </div>
+        <div className="right-side">
+          <div className="profile-card">
+            <img src={userImg} className="user-image" />
+            <h3 className="user-name">David rozario</h3>
+            <div className="follower-info">
+              <h4>40 followers</h4>
+              <h4>30 following</h4>
+            </div>
+            <button className="follow btn-primary">follow</button>
+            <button
+              className="update-profile btn-secondary"
+              onClick={() => navigate("/updateprofile/ghyu")}
+            >
+              Update Profile
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Profile;
