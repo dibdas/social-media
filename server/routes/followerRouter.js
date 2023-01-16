d6c18eba1ad7736097d383afb1c99e0fd126185d
dibdas <dibyandu08das@gmail.com>
2023-01-17 00:57:21 +0530

    modified

--- server/routes/followerRouter.js
+++ server/routes/followerRouter.js
@@ -2,11 +2,13 @@ const userRequire = require("../middlewares/requireUser");
 const {
   followerOrUnFollowUsers,
   getPostFollowingUsers,
+  getMyInfo,
 } = require("../controllers/followersAndFollowing");
 
 const router = require("express").Router();
 router.post("/follow", userRequire, followerOrUnFollowUsers);
 router.get("/getpostsfollowing", userRequire, getPostFollowingUsers);
+router.get("/getMyInfo", userRequire, getMyInfo);
 //deletemyprofile
 //getUserP
 // getMyPost
