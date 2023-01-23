cb6674a5b8185b44b92a46f696a36a3c39c7737c
dibdas <dibyandu08das@gmail.com>
2023-01-23 16:19:28 +0530

    added and modified

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import appConfigSlice from "./slices/appConfigSlice";
+import postsReducer from "./slices/postSlice";
 
 export default configureStore({
   reducer: {
     appConfigReducer: appConfigSlice,
+    postsReducer,
   },
 });
 
