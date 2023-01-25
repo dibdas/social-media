4e58ec7a50f96789c01d33cb216f965ae05cf47d
dibdas <dibyandu08das@gmail.com>
2023-01-25 22:26:32 +0530

    modified

--- client/src/components/feed/Feed.js
+++ client/src/components/feed/Feed.js
@@ -1,35 +1,40 @@
 import React from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getFeedData } from "../../redux/slices/feedSlice";
 import Follower from "../followers/Follower";
 import Post from "../post/Post";
 import "./Feed.scss";
 
 function Feed() {
+  const dispatch = useDispatch();
+  const feedData = useSelector((state) => state.feedReducer.feedData);
+  console.log(feedData);
+
+  useEffect(() => {
+    dispatch(getFeedData());
+  }, [dispatch]);
+
   return (
     <div className="feed">
       <div className="container">
         <div className="left-side">
-          <Post />
-          <Post />
-          <Post />
-          <Post />
-          <Post />
+          {feedData?.fullModifiedPostsOfFollowings?.map((post) => (
+            <Post post={post} key={post?._id} />
+          ))}
         </div>
         <div className="right-side">
           <div className="following">
             <h3 className="title">You are following</h3>
-            <Follower />
-            <Follower />
-            <Follower />
-            <Follower />
-            <Follower />
+            {feedData?.followings?.map((user, id) => (
+              <Follower user={user} key={id} />
+            ))}
           </div>
           <div className="suggesstions">
             <h3 className="title">Suggessted For You</h3>
-            <Follower />
-            <Follower />
-            <Follower />
-            <Follower />
-            <Follower />
+            {feedData?.suggestionUsers?.map((user, id) => (
+              <Follower user={user} key={id} />
+            ))}
           </div>
         </div>
       </div>
--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -14,7 +14,7 @@ function Navbar() {
   const myProfileInfo = useSelector(
     (state) => state.appConfigReducer.myProfile
   );
-  // console.log(myProfileInfo);
+  console.log(myProfileInfo);
   // moving the loadingRef to app.js
   // const loadingRef = useRef(null);
   // const [loading, setLoading] = useState(false);
@@ -37,16 +37,16 @@ function Navbar() {
       {/* <LoadingBar color="#f11946" ref={loadingRef} height={4} /> */}
       <div className="container">
         {/* clicking on Social-Media it will take you or navigate you to the defined path mentioned inside the navigate() */}
-        <h2 className="banner hover-link" onClick={() => navigate("/whatever")}>
+        <h2 className="banner hover-link" onClick={() => navigate("/")}>
           Social-Media
         </h2>
         <div className="right-side">
           <div
             className="profile hover-link"
             // navigating to individual profile id
-            onClick={() => navigate(`/profile/${myProfileInfo?._id}`)}
+            onClick={() => navigate(`/profile/${myProfileInfo?.user?._id}`)}
           >
-            <Avatar src={myProfileInfo?.avatar?.url} />
+            <Avatar src={myProfileInfo?.user?.avatar?.url} />
           </div>
           <div className="logout hover-link" onClick={handleLogoutClicked}>
             <FiLogOut />
