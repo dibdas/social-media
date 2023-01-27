606b5752ba0ad15ba12905dec1b89b674f79d027
dibdas <dibyandu08das@gmail.com>
2023-01-27 20:51:01 +0530

    added HashRouter

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
@@ -13,12 +13,18 @@ root.render(
   //   <App />
   // </React.StrictMode>
 
-  <BrowserRouter>
+  // <BrowserRouter>
+  // Hashrouter is for the production
+  // hashRouter and browserRouter , both are different but intrenally they work in similar fashion
+  // after deployment BrowserRouter does not work properly ,therfore we use HashRouter
+  // with the help of HasHrouter, routes work properly , after the deployment
+  <HashRouter>
     {/* Provider can be written inside or outside the BrowserRouter, they are independent to each other */}
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
+  // </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
