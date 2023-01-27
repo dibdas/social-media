80b5c20238b5e0480c27019b05a3b95f636ade07
dibdas <dibyandu08das@gmail.com>
2023-01-27 18:21:03 +0530

    added bio

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -6,6 +6,7 @@ import { useNavigate, useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import CreatePost from "../createPost/CreatePost";
 import { getUserProfile } from "../../redux/slices/postSlice";
+import { followAndUnfollow } from "../../redux/slices/feedSlice";
 
 function Profile() {
   const myProfileInfo = useSelector(
@@ -21,19 +22,27 @@ function Profile() {
   console.log(userProfile);
   const myProfile = useSelector((state) => state.appConfigReducer.myProfile);
   console.log(myProfile);
+  const feedDatas = useSelector((state) => state.feedReducer.feedData);
   // made this state variable in order to check whether the current user is the loggedIn user or not
   const [isMyProfile, setIsMyProfile] = useState(false);
+  const [isFollowing, setIsFollowing] = useState(false);
 
   useEffect(() => {
+    console.log("profile useeffect");
     dispatch(getUserProfile({ userId }));
     setIsMyProfile(myProfile?.user?._id === userId);
-  }, [myProfile, userId]);
+    setIsFollowing(feedDatas?.followings?.find((item) => item._id === userId));
+  }, [myProfile, userId, feedDatas]);
+
+  async function handleFollowUnfollowUser() {
+    dispatch(followAndUnfollow({ userIdToFollow: userId }));
+  }
 
   return (
     <div className="Profile">
       <div className="container">
         <div className="left-side">
-          <CreatePost />
+          {isMyProfile && <CreatePost />}
           {userProfile?.modifiedPosts?.map((post, id) => (
             <Post post={post} key={id} />
           ))}
@@ -45,6 +54,7 @@ function Profile() {
           <div className="profile-card">
             <img src={userProfile?.avatar?.url} className="user-image" />
             <h3 className="user-name">{userProfile?.name}</h3>
+            <p className="bio">{userProfile?.bio}</p>
             <div className="follower-info">
               <h4>{`${userProfile?.followers?.length}`} followers</h4>
               <h4>{`${userProfile?.followings?.length}`} followings</h4>
@@ -52,7 +62,14 @@ function Profile() {
             {/* if it is not my profile then show the follow button */}
             {/* therefore if the isMyProfile is false i.e not true then show the follow button */}
             {!isMyProfile && (
-              <button className="follow btn-primary">follow</button>
+              <h5
+                className={
+                  isFollowing ? "hover-link follow-link" : "btn-primary"
+                }
+                onClick={handleFollowUnfollowUser}
+              >
+                {isFollowing ? `unfollow` : `follow`}
+              </h5>
             )}
             {isMyProfile && (
               <button
